bcd4f1b5a89c53955b5bb313ac3319398f3c7789
Hyan Mandian <hyan_mandian@sicredi.com.br>
2020-03-13 18:17:16 -0300

    feature: load and resize

---
+++ examples/app-2/index.js
@@ -0,0 +1,10 @@
+async function init() {
+  // const parent = await getParent();
+
+  // // listen to set counter sent by parent
+  // parent.on('set', counter => {
+  //   document.querySelector('[data-counter]').innerHTML = counter;
+  // });
+}
+
+init();
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,64 @@
-import "regenerator-runtime";
-
 class MicroEasy extends HTMLElement {
+  private loaded = false;
+
+  constructor() {
+    super();
+
+    this.style.width = '0px';
+    this.style.height = '0px';
+    this.style.display = 'inline-block';
+    this.style.opacity = '0';
+    this.style.overflow = 'hidden';
+    this.style.position = 'absolute';
+    this.style.visibility = 'hidden';
+    this.style.transition = 'all 0.5s';
+  }
+
+  handleLoad = (e: MessageEvent) => {
+    const { type, payload } = e?.data || {};
+
+    if (
+      type !== 'micro-easy:loaded' ||
+      this.getAttribute('name') === payload.name
+    ) {
+      return;
+    }
+
+    this.style.width = `${payload.size.width}px`;
+    this.style.height = `${payload.size.height}px`;
+    this.style.opacity = '1';
+    this.style.position = 'static';
+    this.style.visibility = 'visible';
+
+    this.loaded = true;
+  };
+
   connectedCallback() {
+    window.addEventListener('message', this.handleLoad);
+
     const src = this.getAttribute('src');
     const name = this.getAttribute('name');
 
-    this.innerHTML = `<iframe src="${src}"></iframe>`;
+    this.innerHTML = `<iframe frameborder="0" name="${name}" src="${src}?micro-easy:name=${name}"></iframe>`;
   }
 
-  static async getParent() {
-    const parent = window.parent;
-
-    return {
-      on(event: string, callback: Function) {},
-      emit(event: string, payload?: any) {},
-    };
-  }
-
-  static async getApplication(name: string) {
-    return {
-      on(event: string, callback: Function) {},
-      emit(event: string, payload?: any) {},
-    };
+  disconnectedCallback() {
+    window.removeEventListener('message', this.handleLoad);
   }
 }
 
 customElements.define('micro-easy', MicroEasy);
 
-window.onload = async function() {
-  window.parent.postMessage("micro-easy:loaded", "*");
+window.onload = function() {
+  const params = new URLSearchParams(window.location.search);
+  const name = params.get('micro-easy:name');
+
+  if (!name) return;
+
+  const size = document.querySelector('html')!.getBoundingClientRect();
+
+  window.parent.postMessage(
+    { type: 'micro-easy:loaded', payload: { name, size } },
+    '*'
+  );
 };
